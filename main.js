639b35aa6a3f56a8418f9c3cf14a322182cdf6dd
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-05-13 16:55:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    canvas = createCanvas(300,230);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iR7mRis69/model.json",modelLoded);
+}
+
+function modelLoded() {
+    console.log(modelLoded);
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify (video , gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_span").innerHTML = results[0].label;
+        document.getElementById("accuracy_span").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
